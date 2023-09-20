cf19e17bb74359359768e8791ef0b1d6a3c8ca48
anasmuostafa <anasmuostafa@gmail.com>
2023-09-20 07:00:23 +0300

    adding simple route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import "./App.css";
 import GenderHomePage from "./pages/GenderHomePage";
 import { women, men, kids } from "./servises/mainpage";
-
+import Header from "./components/Header";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/women" element={<GenderHomePage genderData={women}/>} />
-        <Route path="/men" element={<GenderHomePage genderData={men}/>} />
-        <Route path="/kids" element={<GenderHomePage genderData={kids}/>} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/women"
+            element={<GenderHomePage genderData={women} />}
+          />
+          <Route path="/men" element={<GenderHomePage genderData={men} />} />
+          <Route path="/kids" element={<GenderHomePage genderData={kids} />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 export default App;
