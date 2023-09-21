2fcde44d4239f6c0e754f9b21e2ddaa8ff678a88
User <youssefdaghash864@gamil.com>
2023-09-21 18:03:35 +0200

    i made a sidebar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import "./App.css";
-import GenderHomePage from "./pages/GenderHomePage";
-import { women, men, kids } from "./servises/mainpage";
-import Header from "./components/Header";
-import { Route, Routes } from "react-router-dom";
-import Footer from "./components/footer/Footer";
-import smiFinalFooter from "./components/footer/FooterPart3";
-import verySmallContainer from "./components/footer/FooterPart2";
-import ImgWithText from "./components/footer/ImgWithText";
-import HomePage from "./pages/HomePage";
-import { useState } from "react";
-import ProductsView from "./components/ProductsView/ProductsView";
-import ShoppingCart from "./components/shoppingCart/ShoppingCart";
-import Man from "./pages/Man";
+import  "./App.css";
+import SideBar from "./components/sidebar/SideBar";
 function App() {
-  let [genderChoosed, setGenderChoosed] = useState(0);
   return (
     <>
-      <Header genderChoosed = {genderChoosed} setGenderChoosed ={setGenderChoosed}  />
-        <Routes>
-          <Route path="/" element={<HomePage setGenderChoosed ={setGenderChoosed} />} />
-          <Route
-            path="/women"
-            element={<GenderHomePage genderData={women} />}
-          />
-          <Route path="/men" element={<GenderHomePage genderData={men} />} />
-          <Route path="/kids" element={<GenderHomePage genderData={kids} />} />
-          <Route path="/products" element={<ProductsView />} />
-          <Route path="/cart" element={<ShoppingCart />} />
-        </Routes>
-        <Footer />
+       <SideBar />
     </>
   );
 }
 export default App;
+
--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -1,7 +1,71 @@
 export default function SideBar(){
+    let arr1=[" Blazer Jacket","  Athlete"," Boxer"," Cardigan","Coat"]
+    let arr2 =[ "46","48","50","52","54"]
+    let arr3=["Blue","Bordeaux","Anthracite","Beige","Black"]
+    
+
     return (
-        <>
-            
-        </>
+       <div className="">   
+        <div className="ml-5 ">
+        {arr1.map((v, index) => (
+            <>
+            <div className="flex ml-5">
+            <input type="checkbox" />
+            <p
+                className="ml-2"
+                key={index}
+              >
+                {v}
+              </p>
+              </div>
+              </>
+            ))}
+
+        </div>
+        <div className="ml-5 mt-4">
+        {arr2.map((v, index) => (
+            <>
+            <div className="flex ml-5">
+            <input type="checkbox" />
+            <p
+                className="ml-2"
+                key={index}
+              >
+                {v}
+              </p>
+              </div>
+              </>
+            ))}
+        </div> 
+        <div className="ml-5 mt-5">
+        {arr3.map((v, index) => (
+            <>
+            <div className="flex ml-5 ">
+            <input type="checkbox" />
+            <p
+                className="ml-2"
+                key={index}
+              >
+                {v}
+              </p>
+              </div>
+              </>
+            ))}
+        </div>      
+        <div className="flex ml-3">
+        <div className="w-20"> 
+          <p>
+            from
+          </p>
+          <input className="w-20 border border-black rounded" type="number" />
+        </div>
+        <div className=" w-20 ml-3">
+          <p>
+            to
+          </p>
+          <input className="w-20 border border-black rounded" type="number" />
+        </div>
+        </div>
+       </div>
     )
 }
