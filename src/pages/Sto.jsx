9d87c1d75efbc97181d982a6bbc870bc58232c3e
anasmuostafa <anasmuostafa@gmail.com>
2023-09-27 03:38:35 +0300

    adding functionality to stores page

--- src/pages/Sto.jsx
+++ src/pages/Sto.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Sto = () => {
+  const [City, setCity] = useState("Al Menofiah");
+
   const stores = [
     "Select All",
     "ALEXANDRIA AGAMY MALL",
@@ -38,7 +40,7 @@ export const Sto = () => {
 
   function cityCard(city_name) {
     return (
-      <div class="flex m-1 group">
+      <div class="flex m-1 group" onClick={() => setCity(city_name)}>
         <div class="bg-slate-100 w-28 py-3 text-center border group-hover:border-black">
           <p>{city_name}</p>
         </div>
@@ -100,14 +102,90 @@ export const Sto = () => {
         <div className=" md:w-full w-60">
           <h3 className=" font-semibold">جمهوريه مصر العربيه/ALL STORES </h3>
           <p>We have 26 stores at جمهوريه مصر العربيه</p>
-          <iframe
-            className="w-60  md:w-full"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d874323.6119730456!2d31.465105616895407!3d31.128443102191298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145875f6592ee989%3A0xa0f7a3872335c0ce!2z2KjZhtmH2KfYjCDZgtiz2YUg2KjZhtmH2KfYjCDYqNmG2YfYp9iMINin2YTZgtmE2YrZiNio2YrYqQ!5e0!3m2!1sar!2seg!4v1694990736984!5m2!1sar!2seg"
-            height="450"
-            allowfullscreen=""
-            loading="lazy"
-            referrerpolicy="no-referrer-when-downgrade"
-          ></iframe>
+
+          {/* alex */}
+          {City === "Alexandria" && (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d41119.37455674495!2d29.917571526242185!3d31.245421226337815!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14f5c57367e76367%3A0x178318efd40da410!2sDefacto%20store!5e0!3m2!1sen!2seg!4v1695763274783!5m2!1sen!2seg"
+              height="450"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+              className="w-full"
+            ></iframe>
+          )}
+
+          {/* Al Menofiah */}
+          {City === "Al Menofiah" && (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3435.3117650101785!2d31.004719375664894!3d30.568743274663635!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14f7d74bd66cb87d%3A0xb5140f609dad0d3b!2zRGVmYWN0byBTaG9wINmB2LHYuSDYr9mK2YHYp9mD2KrZiCDYtNio2YrZhg!5e0!3m2!1sen!2seg!4v1695763615191!5m2!1sen!2seg"
+              className="w-full"
+              height="450"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          )}
+
+          {/* cairo */}
+          {City === "Cairo" && (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d440458.11280685436!2d30.988238734514585!3d30.405500237123313!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583e6f259cf819%3A0x2f8f99416c8df5a5!2sDeFacto!5e0!3m2!1sen!2seg!4v1695763713993!5m2!1sen!2seg"
+              className="w-full"
+              height="450"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          )}
+
+          {/* Dakahlia */}
+          {City === "Dakahlia" && (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d219022.27843015885!2d31.040744760408277!3d30.93633297845519!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14f79dd2c91a7c37%3A0x3e4ee0c8da2ebd17!2sDeFacto%20Mansoura%20Downtown%20Mall!5e0!3m2!1sen!2seg!4v1695763784923!5m2!1sen!2seg"
+              className="w-full"
+              height="450"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          )}
+
+          {/* giza */}
+          {City === "Giza" && (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110553.52820247269!2d31.005678591419375!3d30.013963085187072!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x145846cc14e07c3b%3A0x273d4a882e038952!2sDe%20Facto!5e0!3m2!1sen!2seg!4v1695763826706!5m2!1sen!2seg"
+              className="w-full"
+              height="450"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          )}
+
+          {/* Qaliubia */}
+          {City === "Qaliubia" && (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d110373.56998525244!2d31.323665597265624!3d30.175021400000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14581a58e247aab3%3A0xb21f75f66b53ef3d!2sDeFacto!5e0!3m2!1sen!2seg!4v1695763889642!5m2!1sen!2seg"
+              className="w-full"
+              height="450"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          )}
+
+          {/* tanta */}
+          {City === "Tanta" && (
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27410.20041528338!2d30.9997812497336!3d30.82295822058321!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14f7b77f6fbf3983%3A0x925d797738e922c8!2sDefacto!5e0!3m2!1sen!2seg!4v1695763944446!5m2!1sen!2seg"
+              className="w-full"
+              height="450"
+              allowfullscreen=""
+              loading="lazy"
+              referrerpolicy="no-referrer-when-downgrade"
+            ></iframe>
+          )}
         </div>
         <div>
           <div className="flex m-2 flex-wrap ho">
